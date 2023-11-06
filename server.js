69d638402461d53005ab694bd4de1a3471a3518e
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-11-06 15:39:47 +0900

    11/6 aws 이미지 업로드 버킷 저장

--- server.js
+++ server.js
@@ -36,6 +36,27 @@ app.use(session({
 
 app.use(passport.session()) 
 
+const { S3Client } = require('@aws-sdk/client-s3')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const s3 = new S3Client({
+    region : 'ap-northeast-2',
+    credentials : {
+        accessKeyId : process.env.S3_KEY,
+        secretAccessKey : process.env.S3_SECRET
+    }
+    })
+
+    const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'jongheonforum1',
+        key: function (요청, file, cb) {
+        cb(null, Date.now().toString()) //업로드시 파일명 변경가능
+        }
+    })
+})
+
 
 let db;
 const url = 'mongodb+srv://whdgjs7300:qwer1234@cluster0.ef3bhk8.mongodb.net/?retryWrites=true&w=majority'
@@ -86,7 +107,8 @@ app.get('/write', (요청, 응답) => {
 }) 
 
 
-app.post('/add', async(요청, 응답) => {
+app.post('/add',upload.single('img1') ,async(요청, 응답) => {
+    console.log(요청.file)
     // 유저가 보낸 데이터 확인
     console.log(요청.body)
     try {
@@ -94,7 +116,9 @@ app.post('/add', async(요청, 응답) => {
             응답.send('제목 입력하세요')
         }else {
             // 유저가 보낸 데이터 DB에 저장
-            await db.collection('post').insertOne({title : 요청.body.title, content: 요청.body.content})
+            await db.collection('post').insertOne({title : 요청.body.title, content: 요청.body.content,
+            img : 요청.file.location
+            })
             // 서버 기능이 끝나면 항상 응답을 해줘야함 !!
             // redirect는 요청이 끝나면 해당 url로 이동시킴
             응답.redirect('/list')
